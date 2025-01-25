8263db7813d5ab97f4256a38e303f440aaac7c82
NotAlexNoyle <alexnoyle@icloud.com>
2025-01-25 11:03:30 -0500

    Rebase on upstream

--- src/main/java/pl/ibcgames/mclvotifier/Votifier.java
+++ src/main/java/pl/ibcgames/mclvotifier/Votifier.java
@@ -1,49 +1,76 @@
 package pl.ibcgames.mclvotifier;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-import pl.ibcgames.mclvotifier.Modules.Configuration;
-
 import java.util.logging.Logger;
 
-public final class Votifier extends JavaPlugin {
-    public static Logger log = Bukkit.getLogger();
-    public static Votifier plugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
-    public static Configuration Config;
+import pl.ibcgames.mclvotifier.Modules.Configuration;
+
+public final class Votifier extends JavaPlugin {
+
+    private static Logger log = Bukkit.getLogger();
+    private static Votifier plugin;
+    private static Configuration Config;
 
     @Override
     public void onEnable() {
+
         plugin = this;
-        Config = new Configuration(this);
+
+        Config = (new Configuration(this));
         this.saveDefaultConfig();
 
-        String token = this.plugin.getConfiguration().get().getString("server_id");
-
+        String token = getPlugin().getConfiguration().get().getString("server_id");
         if (token == null || token.equalsIgnoreCase("paste_server_id_here")) {
+
             this.warning("No server id found in MCL-Votifier config");
             this.warning("How to use this plugin? See tutorial at:");
             this.warning("https://minecraft-servers.gg/mcl-votifier-plugin");
+
         }
 
         this.getCommand("mcl-vote").setExecutor(new Vote());
         this.getCommand("mcl-reward").setExecutor(new Reward());
         this.getCommand("mcl-test").setExecutor(new Test());
+
     }
 
     @Override
     public void onDisable() {
+
+        this.warning("MCL-Votifier-OG shut down successfully.");
+
     }
 
     public Configuration getConfiguration() {
-        return this.Config;
+
+        return Config;
+
     }
 
     public void log(String log) {
-        plugin.log.info("[MCL-Votifier] " + log);
+
+        getLog().info("[MCL-Votifier-OG] " + log);
+
     }
 
     public void warning(String log) {
-        plugin.log.warning("[MCL-Votifier] " + log);
+
+        getLog().warning("[MCL-Votifier-OG] " + log);
+
     }
+
+    public static Votifier getPlugin() {
+
+        return plugin;
+
+    }
+
+    public static Logger getLog() {
+
+        return log;
+
+    }
+
 }
